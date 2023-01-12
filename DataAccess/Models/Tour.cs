4c7a47e471e6ad87cf64c565022144f85846bc6a
Admir S <admir.s@unvi.edu.ba>
2023-01-12 09:23:55 +0100

    Add project files.

---
+++ DataAccess/Models/Tour.cs
@@ -0,0 +1,28 @@
+using Microsoft.Build.Framework;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace ExploreCalifornia.DataAccess.Models
+{
+    public class Tour
+    {
+        public int TourId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal Price { get; set; }
+
+        [Column(TypeName = "varchar(MAX)")]
+        public string Notes { get; set; }
+
+        public List<Reservation> Reservations { get; set; }
+    }
+}
